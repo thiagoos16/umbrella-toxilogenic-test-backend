783fee598c19fad185729838d5f2ab759f1ffa26
thiago.oliveira <thiago.oliveira@intera.org.br>
2021-06-01 18:02:36 -0400

    add mongodb connection

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,17 @@ require('dotenv').config();
 import * as http from 'http';
 import Api from './config/api'
 import { config } from './config/conf'
+import mongoose from 'mongoose';
 
 const server = http.createServer(Api);
+
+mongoose.connect(
+    config.dbURL + config.db, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    }
+);
 
 server.listen(config.serverPort, () => console.log(`Server in running at port ${config.serverPort}`));
